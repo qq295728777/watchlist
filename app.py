aa510154c163facfca67818bdc99d7ef150b07be
sun fang <295728777@qq.com>
2019-04-07 21:25:05 +0800

    I'm ready again!

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
-from flask import Flask, render_template
+import os
+import sys
+from flask import Flask
 
-name = 'Grey Li'
-movies = [
-	{'title': 'My Neighbor Totoro', 'year': '1988'},
-	{'title': 'Dead Poets Society', 'year': '1989'},
-	{'title': 'A Perfect World', 'year': '1993'},
-	{'title': 'Leon', 'year': '1994'},
-	{'title': 'Mahjong', 'year': '1996'},
-	{'title': 'Swallowtail Butterfly', 'year': '1996'},
-	{'title': 'King of Comedy', 'year': '1999'},
-	{'title': 'Devils on the Doorstep', 'year': '1999'},
-	{'title': 'WALL-E', 'year': '2008'},
-	{'title': 'The Pork of Music', 'year': '2012'},
-]
+WIN = sys.platform.startswith('win')
+if WIN:
+	prefix = 'sqlite:///'
+else:
+	prefix = 'sqlite:///'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
-def index():
-	return render_template('index.html', name=name, movies=movies)
-
-@app.route('/ss')
-def get_image():
-	return render_template('index1.html', name=name, movies=movies)
+db = SQLAlchemy(app)
